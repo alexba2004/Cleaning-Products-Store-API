4006ea81dfd5a6deb1e04ee4bedf0b04905f2e9c
Briones <j.alejandro.briones.a22@gmail.com>
2023-12-10 22:46:59 -0600

    fix: UX/UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/views/**/*.pug"],
-    theme: {
-        extend: {
-            colors: {
-                customBlue: "#026873",
-                customBlue2: "#01343A",
-                customYellow: "#BF8C2C",
-                customYellow2: "#9F7526",
-                customGreen1: "#6DC799",
-                customGreen2: "#2D523F",
-                customGreen3: "#2BA868",
-                customGreen4: "#437A5E",
-                customGreen5: "#87F5BC",
-            },
-            fontFamily: {
-                Custom: ["Roboto", "Arial", "sans-serif"],
-            },
-        },
+  content: ["./src/views/**/*.pug"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "#1797A6",
+        customBlue2: "#1797A6",
+        customYellow: "#F2921D",
+        customYellow2: "#F28705",
+        customGreen1: "#318EAD",
+        customGreen2: "#212226",
+        customGreen3: "#F2921D",
+        customGreen4: "#F28705",
+        customGreen5: "#FFFFFF",
+        customColor: "#BF0413",
+        customBlue12: "#0C214E",
+        customBlue13: "#071A26",
+        customBulue14: "#BFBFBF",
+        customBlue15: "#76CAEB",
+        customBlue16: "#3CB03E",
+        customBlue17: "#C92724",
+      },
+      fontFamily: {
+        Custom: ["Roboto", "Arial", "sans-serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
