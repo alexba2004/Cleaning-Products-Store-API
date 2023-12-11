ccefb5f9503ba344303072fd27c5e45f4ef56ce9
Briones <j.alejandro.briones.a22@gmail.com>
2023-12-11 05:43:11 -0600

    fix: edit view

--- src/lib/seeders/productSeed.js
+++ src/lib/seeders/productSeed.js
@@ -1,33 +1,33 @@
 // Array de productos con sus datos
 const productsData = [
-    {
-        productName: "Producto 1",
-        productDescription: "Descripcion 1",
-        price: 10.99,
-        status: true,
-        productImage: "/img/products/producto1.png",
-    },
-    {
-        productName: "Producto 2",
-        productDescription: "Descripcion 2",
-        price: 20.99,
-        status: true,
-        productImage: "/img/products/producto2.png",
-    },
-    {
-        productName: "Producto 3",
-        productDescription: "Descripcion 3",
-        price: 15.99,
-        status: true,
-        productImage: "/img/products/producto3.png",
-    },
-    {
-        productName: "Producto 4",
-        productDescription: "Descripcion 4",
-        price: 12.99,
-        status: true,
-        productImage: "/img/products/producto4.png",
-    },
+  {
+    productName: "Pinolimp",
+    productDescription: "Limpiador líquido multiusos, color verde esmeralda, lechoso, útil en la limpieza de pisos, paredes, muebles de baño y otras superficies.",
+    price: 10.99,
+    status: true,
+    productImage: "/img/products/producto1.png",
+  },
+  {
+    productName: "Producto 2",
+    productDescription: "Descripcion 2",
+    price: 20.99,
+    status: true,
+    productImage: "/img/products/producto2.png",
+  },
+  {
+    productName: "Producto 3",
+    productDescription: "Descripcion 3",
+    price: 15.99,
+    status: true,
+    productImage: "/img/products/producto3.png",
+  },
+  {
+    productName: "Producto 4",
+    productDescription: "Descripcion 4",
+    price: 12.99,
+    status: true,
+    productImage: "/img/products/producto4.png",
+  },
 ];
 
 export default productsData;
